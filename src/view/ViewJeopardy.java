3e1e29238af0ee78a09f8dcdf3c121d587b63296
David nin <daviiidniiin@gmail.com>
2019-03-24 12:52:44 +0100

    Doing the views

--- src/view/ViewJeopardy.java
+++ src/view/ViewJeopardy.java
@@ -3,24 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package jeopardy;
-
-import view.VentanaPreguntasGUI;
-
-
-
+package view;
 
 /**
  *
  * @author usuario
  */
-public class JEOPARDY {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        
-    }
+public class ViewJeopardy {
     
 }
